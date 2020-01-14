6034bcf5b17441de59688e85c698b700a4ff0d2e
mstoiovici <mirela.stoiovici@ada.ac.uk>
2020-01-14 13:27:41 +0000

    add subtract test and method to class

--- test/coolculator.test.js
+++ test/coolculator.test.js
@@ -18,8 +18,8 @@ describe('Coolculator', function() {
   })
 
   // Uncomment this 👇
-  // it('should subtract', function() {
-  //   result = mm.subtract(3, 2)
-  //   expect(result).to.equal(1)
-  // })
+  it('should subtract', function() {
+    result = mm.subtract(3, 2)
+    expect(result).to.equal(1)
+  })
 })
