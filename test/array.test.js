d0299b524da3df4404172f05503f75f3b4cda699
mstoiovici <mirela.stoiovici@ada.ac.uk>
2020-01-14 12:24:12 +0000

    added pop function

--- test/array.test.js
+++ test/array.test.js
@@ -17,3 +17,19 @@ describe('Array', function() {
     })
   })
 })
+
+describe('Array', function() {
+  describe('#pop()', function() {
+    it('should remove an item from the array', function() {
+      var array = [1, 2, 3, 4]
+      array.pop(4)
+      expect(array).to.deep.equal([1, 2, 3])
+    })
+
+    it('should alter the length properly', function() {
+      var array = [1, 1, 1, 1]
+      array.pop(1)
+      expect(array.length).to.equal(3)
+    })
+  })
+})
